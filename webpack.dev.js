0badd454e4dde735bb3c28ce08d1b75769968768
Emanuel Marin <emanuel.marin0000@gmail.com>
2025-10-04 18:41:48 +0300

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    watchFiles: ['./src/template.html'],
+    hot: true,
+    open: true,
+  },
+});

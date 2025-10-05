4a0fbc1ebebe644fc440f84e737083d9caf71ac3
emianuelul <emanuel.marin0000@gmail.com>
2025-10-05 13:28:44 +0300

    can now make requests for a given location in code, displays current temp in console

---
+++ src/Weather/WeatherProcessor.js
@@ -0,0 +1,23 @@
+import { format } from 'date-fns';
+export default class WeatherProcessor {
+  static #API_KEY = '8CYFEA2MNNBQD7YG5C6YDKB5K';
+  static #API_KEY_ENDPOINT = '?key=' + this.#API_KEY;
+  static #STARTING_LINK =
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+
+  static fahToCel(fah) {
+    return (fah - 32) / (9 / 5);
+  }
+
+  static async getLocationJSON(location) {
+    const link = this.#STARTING_LINK + location + this.#API_KEY_ENDPOINT;
+
+    try {
+      const data = await fetch(link);
+      const json = await data.json();
+      return json;
+    } catch (err) {
+      console.error('Erorr: ', err);
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import DomStuff from './DomStuff/DomStuff';
+import WeatherProcessor from './Weather/WeatherProcessor';
+import { format } from 'date-fns';
 
 const processor = (function () {
   const app = document.getElementById('app');
@@ -6,4 +8,16 @@ const processor = (function () {
 
   const div = DomStuff.createDiv('.dick');
   app.appendChild(div);
+
+  const processWeatherJSON = async () => {
+    const locationData = await WeatherProcessor.getLocationJSON('Romania');
+    console.log(locationData);
+    console.log(
+      `It is ${WeatherProcessor.fahToCel(
+        locationData.currentConditions.temp
+      ).toFixed(1)}C in Romania`
+    );
+  };
+
+  processWeatherJSON();
 })();
